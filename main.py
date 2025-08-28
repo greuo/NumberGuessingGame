b53da0ba29cddcae29fb386481c8189b9a5bfcd9
Greuo <0101swj@gmail.com>
2025-08-28 11:38:47 +0900

    Added input validation Y/N is no longer case sensitive

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 import random
 import sys
 
+tries = 0
+
+def INPUT():
+    tmp = input(f"This is attempt number {tries}: ")
+    if tmp.isdigit():
+        return int(tmp)
+    else:
+        print("Please enter a number")
+        return INPUT()
+
 def playGame():
     print("you have 10 tries to guess a number between 1 and 1000")
     number = random.randint(1, 1000)
+    global tries
     tries = 0
     while tries < 10:
         tries += 1
-        tmp = int(input(f"This is guess number {tries}: "))
+        tmp = INPUT()
         if tmp < number:
             print("Too small!")
-        if tmp > number:
+        elif tmp > number:
             print("Too big!")
-        if tmp == number:
+        else:
             print("congratulations! You guessed the right number!")
-            playAgain = input("Would you like to play again? (Y/N): ")
-            if playAgain == 'Y':
+            playAgain = input("Would you like to play again? (Y/N): ").strip().lower()
+            if playAgain == 'y':
                 playGame()
             else:
                 print("Program ending...")
                 sys.exit(0)
-    restart = input("That was your last try. Do you want to restart? (Y/N): ")
-    if restart == 'Y':
+    restart = input("That was your last try. Do you want to restart? (Y/N): ").strip().lower()
+    if restart == 'y':
         playGame()
     else:
         print("Program ending...")
